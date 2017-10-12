b96e986df335e776413fc9e771d66f7790e7bdb9
H.Nagao <gen1218@gmail.com>
2017-10-12 23:19:25 +0900

    Models

---
+++ Models/BindingClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+
+
+namespace CommentBoardRefine.Models
+{
+  public  class BindingClass
+    {
+        public ObservableCollection<Column> BindModel { get; set; }
+
+         public   BindingClass()
+            {
+            string InsertQuery= "SELECT COMMENT_ID, USER_NAME,MESSAGE,POSTED_DATE,END_DATE FROM COMMENTBOARD "                                                   
+                          +"WHERE 1=1 "
+                          +"  AND CONVERT(DATE, END_DATE) >= CONVERT(DATE, GETDATE()) "                          
+                          +"ORDER BY POSTED_DATE DESC";
+
+            DataTable GetDatatable = SQL.GetDB(InsertQuery);
+
+            BindModel = new ObservableCollection<Column>() { };
+            
+            for (int i = 0; i < GetDatatable.Rows.Count; i++)
+            {
+                Column cl = new Column();
+
+                cl.ID = (int)GetDatatable.Rows[i].ItemArray[0];
+                cl.User = GetDatatable.Rows[i].ItemArray[1].ToString();
+                cl.Message= GetDatatable.Rows[i].ItemArray[2].ToString();
+                cl.Posted_Date = (DateTime)GetDatatable.Rows[i].ItemArray[3];
+                cl.End_Date = (DateTime)GetDatatable.Rows[i].ItemArray[4];
+
+                BindModel.Add(cl);
+                                
+            }  
+        }
+        
+    }
+}
---
+++ Models/MainWindowControl.cs
@@ -0,0 +1,26 @@
+﻿
+using Livet;
+using CommentBoardRefine.Views;
+using CommentBoardRefine.ViewModels;
+using System.Windows;
+
+namespace CommentBoardRefine.Models
+{
+    public class MainWindowControlModel : NotificationObject
+    {
+        public static void CreatePostWindow(MainWindowViewModel Parent)
+        {
+            var PWindow = new PostWindow();
+            
+            PostWindowViewModel vm = (PostWindowViewModel)PWindow.DataContext;
+            vm.ParentViewModel = Parent;
+
+            PWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            PWindow.Show();
+
+
+
+        }
+
+    }
+}
