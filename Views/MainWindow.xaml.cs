37e357a999a15ec3328eefbf995c6dfa51c609b7
H.Nagao <gen1218@gmail.com>
2017-12-10 22:10:16 +0900

    Update MainWindow.xaml.cs

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ namespace CommentBoardRefine.Views
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
+    //  Viewのコードビハインドは原則記述なし
     public partial class MainWindow : Window
     {
         public MainWindow()
