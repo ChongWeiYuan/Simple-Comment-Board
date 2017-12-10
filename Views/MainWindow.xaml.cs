98edcf38b520788fb45bc70c3ce056134e952f53
H.Nagao <gen1218@gmail.com>
2017-12-10 21:44:33 +0900

    Update MainWindow.xaml.cs

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -21,8 +21,7 @@ namespace CommentBoardRefine.Views
     {
         public MainWindow()
         {
-            InitializeComponent();
-            
+            InitializeComponent();            
         }
     }
 }
