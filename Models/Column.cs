497e00589e65557af3a89a74068524db0ddc3f4d
H.Nagao <gen1218@gmail.com>
2018-01-09 19:13:44 +0900

    Update Column.cs

--- Models/Column.cs
+++ Models/Column.cs
@@ -7,10 +7,8 @@ namespace CommentBoardRefine.Models
     {
         public int ID { get; set; }
         public string Message { get; set; }
-
         public string User { get; set; }
         public DateTime Posted_Date { get; set; }
         public DateTime End_Date { get; set; }
-
     }
 }
